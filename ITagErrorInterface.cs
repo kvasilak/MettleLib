eb620d4e2ce70ad9ca0ee2e46ab8a6a18c5e8ea4
keith.vasilakes <keith.vasilakes@spectrumdsi.com>
2013-12-14 13:36:25 -0600

    create library

---
+++ ITagErrorInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MettleLib
+{
+    public interface ITagErrorInterface
+    {
+        void UpdateEvent(string s);
+        void Initialize();
+        void Reset();
+    }
+}
---
+++ TagInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MettleLib
+{
+    public interface ITagInterface
+    {
+        void UpdateEvent(TagEvent e);
+        void Initialize();
+        void Reset();
+    }
+}
